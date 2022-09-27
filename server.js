5a47c0e9d04cc5efe62f7fa73d1b27ac2a4c5681
devashis <devg619@gmail.com>
2022-09-27 20:16:10 +0545

    v1.0.0

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ if(process.env.NODE_ENV === 'production')
 {
     app.use(express.static("client/build"));
 }
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000 ;
+
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
